46c6ec830c66376139ee27078e70d98658920ea0
ketan <ketanmohite8307@gmail.com>
2022-05-07 19:47:06 +0530

    Updates

---
+++ precache-manifest.90aa254f7f92ffe761bb9226e3f960aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ad669bb2292ebced51f",
+    "url": "/finalunit6/static/css/main.505c9bf7.chunk.css"
+  },
+  {
+    "revision": "5ad669bb2292ebced51f",
+    "url": "/finalunit6/static/js/main.04b002d6.chunk.js"
+  },
+  {
+    "revision": "5f00ad19c1c970aec863",
+    "url": "/finalunit6/static/js/runtime~main.958c46d6.js"
+  },
+  {
+    "revision": "6121fd25cb06ddf638d7",
+    "url": "/finalunit6/static/js/2.6e7024b3.chunk.js"
+  },
+  {
+    "revision": "0222b4ed3fafae3e17ab",
+    "url": "/finalunit6/static/js/3.50b2d2e5.chunk.js"
+  },
+  {
+    "revision": "b7f84d4aeefa4e6c09b59b26710a56ca",
+    "url": "/finalunit6/index.html"
+  }
+];
